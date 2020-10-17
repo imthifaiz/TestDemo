263f38aa859b95e970b06dde8522a7e4ce2a78f6
imthifaiz <70131253+imthifaiz@users.noreply.github.com>
2020-10-18 02:40:39 +0530

    Update App.java

--- src/main/java/com/demo/spring/githundemo/App.java
+++ src/main/java/com/demo/spring/githundemo/App.java
@@ -1,13 +1,26 @@
-package com.demo.spring.githundemo;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class Sample {
+	public static void main(String args[]) {
+		String[] Dept = {"CSE", "ECE", "CIVIL", "MECH"};
+		for (String k : Dept)
+		{
+			System.out.println("\n			<<--------"+k+" DEPARTMENT-------->>");
+			for (int i=1; i<=1; i++)
+			{
+			System.out.println("___________________________________class "+i+" "+k+"___________________________________");
+				for (int j=1; j<=2; j++)
+				{
+				System.out.println("\n**********STUDENT "+j+"**********");
+				Scanner input = new Scanner(System.in);
+				System.out.print("Enter Name: ");
+				String name = input.nextLine();
+				System.out.print("Enter Roll Number: ");
+				String roll = input.nextLine();
+				System.out.println("STUDENT NAME IS : "+name + " ||ROLL NUMBER IS : "+roll);
+				}
+				System.out.println("____________________________________________________________________________________________________________________________________________________________________________________________________________");
+			}
+		}
+	}
 }
